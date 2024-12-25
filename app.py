b79f21058b334951ec91ae9355fa9df29810ebb2
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2024-12-25 15:22:54 -0600

    Update app.py
    added photo input

--- app.py
+++ app.py
@@ -49,8 +49,14 @@ def process_elements(elements, related_elements):
 
 def main():
     st.title('Dividir Lista en Sublistas Aleatorias y Relacionarlas')
-    st.subheader('Elementos añadidos')
-    elements = st.data_editor(["Default"], num_rows="dynamic", use_container_width=True, key='elements_editor')
+    st.subheader('Foto')
+    enable = st.checkbox("Enable camera")
+    picture = st.camera_input("Take a picture", disabled=not enable)
+
+    if picture:
+        st.image(picture)
+        st.subheader('Elementos añadidos')
+        elements = st.data_editor(["Default"], num_rows="dynamic", use_container_width=True, key='elements_editor')
     st.subheader('Elementos relacionados añadidos')
     related_elements = st.data_editor(["Default1"], num_rows="dynamic", use_container_width=True, key='related_elements_editor')
 
